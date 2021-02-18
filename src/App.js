71c9dd4a46effba9647df305c49cd0402aed186a
Rich Korneisel <korneisel@cfu.net>
2021-02-18 13:23:53 -0600

    After homework review

--- src/App.js
+++ src/App.js
@@ -1,68 +1,69 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import Receipt from "./Receipt"
-import './Receipt.css';
+import Receipt from './Receipt';
 
-const receipt1 = {
-  person: "Karolin",
-  order: {
-    main: "Burrito",
-    protein: "Organic Tofu",
-    rice: "Purple Rice",
-    sauce: "Green Crack",
-    toppings: ["Baby Bok Choy", "Cucumber Kimchi"],
-    drink: "Korchata",
-    cost: 22
-  },
-  paid: false
-};
-const receipt2 = {
-  person: "Jerrica",
-  order: {
-    main: "Rice Bowl",
-    protein: "Ginger Soy Chix",
-    rice: "Sticky Rice",
-    sauce: "Korilla",
-    toppings: ["Yuzu Pickled Sweet Pepper", "Kale"],
-    drink: "Korchata",
-    cost: 19
-  },
-  paid: false
-};
-const receipt3 = {
-  person: "Matt",
-  order: {
-    main: "Salad Bowl",
-    protein: "Organic Tofu",
-    rice: "none",
-    sauce: "K'lla",
-    toppings: ["Blue Potato Salad", "Pico De Gallo", "Red Kimchi"],
-    drink: "Sparkling Blood Orange Soda",
-    cost: 20
-  },
-  paid: false
-};
-
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      receipt1: receipt1,
-      receipt2: receipt2,
-      receipt3: receipt3,
+      receipts: [
+        {
+          person: "Karolin",
+          order: {
+            main: "Burrito",
+            protein: "Organic Tofu",
+            rice: "Purple Rice",
+            sauce: "Green Crack",
+            toppings: ["Baby Bok Choy", "Cucumber Kimchi"],
+            drink: "Korchata",
+            cost: 22
+          },
+          paid: false
+        },
+        {
+          person: "Jerrica",
+          order: {
+            main: "Rice Bowl",
+            protein: "Ginger Soy Chix",
+            rice: "Sticky Rice",
+            sauce: "Korilla",
+            toppings: ["Yuzu Pickled Sweet Pepper", "Kale"],
+            drink: "Korchata",
+            cost: 19
+          },
+          paid: false
+        },
+        {
+          person: "Matt",
+          order: {
+            main: "Salad Bowl",
+            protein: "Organic Tofu",
+            rice: "none",
+            sauce: "K'lla",
+            toppings: ["Blue Potato Salad", "Pico De Gallo", "Red Kimchi"],
+            drink: "Sparkling Blood Orange Soda",
+            cost: 20
+          },
+          paid: false
+        }
+      ]
+    }
   }
-}
-  render () {
+  render() {
+    const allReceipt = this.state.receipts.map((receiptElement, index) => {
+      // condition that valides paid receipts
+      if (receiptElement.paid === false) {
+        return <Receipt receipt={receiptElement} key={index} />
+      }
+    })
     return (
-      <div>
-        <h1 className="truck-name">Korilla Receipts</h1>
-        <Receipt receipt={this.state.receipt1}/>
-        <Receipt receipt={this.state.receipt2}/>
-        <Receipt receipt={this.state.receipt3}/>
-        
-      </div>
+      <div className="App">        <h1 className="truck-name">Korilla Receipts</h1>        <div className="container">          {allReceipt}          {/* {this.state.receipt1.paid===false ?
+            <Receipt receipt={this.state.receipt1}/>
+          :
+            null
+          }          <Receipt receipt={this.state.receipt2}/>
+          <Receipt receipt={this.state.receipt3}/> */}
+      </div>      </div>
     )
   }
-}
-
-export default App;
+} export default App;
--- src/Receipt.js
+++ src/Receipt.js
@@ -9,37 +9,17 @@ class Receipt extends React.Component {
     render(){
         console.log(this.props.receipt)
         return(
-            <ul className="receipt">
-                <li><span>Person:</span> {this.props.receipt.person}</li>
-                <li><span>Order</span></li>
-                <li><span>Main:</span> {this.props.receipt.order.main}</li>
-                <li><span>Protein:</span> {this.props.receipt.order.protein}</li>
-                <li><span>Rice:</span> {this.props.receipt.order.rice}</li>
-                <li><span>Sauce:</span> {this.props.receipt.order.sauce}</li>
-                <li><span>Drink:</span> {this.props.receipt.order.drink}</li>
-                <li><span>Cost:</span> {this.props.receipt.order.cost}</li>
-            </ul>
+            <div className="receipt">
+                <h3>{this.props.receipt.person}</h3>
+                <p><span>Main:</span> {this.props.receipt.order.main}</p>
+                <p><span>Protein:</span> {this.props.receipt.order.protein}</p>
+                <p><span>Rice:</span> {this.props.receipt.order.rice}</p>
+                <p><span>Sauce:</span> {this.props.receipt.order.sauce}</p>
+                <p><span>Drink:</span> {this.props.receipt.order.drink}</p>
+                <p><span>Cost:</span> {this.props.receipt.order.cost}</p>
+            </div>
         )
     }
 }
-
-
-// function Receipt (props) {
-// return (
-//     <div className="Receipt">
-//         <div>
-//             <li><span>Person</span>{this.state.person}​​</li>
-//                 <ul>
-//                     <li><span>Main</span>{this.state.order.main}​​</li>
-//                     <li><span>Protein</span>{this.state.order.protein}​​</li>
-//                     <li><span>Rice</span>{this.state.order.rice}​​</li>
-//                     <li><span>Sauce</span>{this.state.order.sauce}​​</li>
-//                     <li><span>Drink</span>{this.state.order.drink}​​</li>
-//                     <li><span>Order</span>{this.state.order.cost}​​</li>
-//                 </ul>
-//         </div>
-//     </div>
-//     )
-// }
 
 export default Receipt;
