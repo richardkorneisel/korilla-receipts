27f0240fa1f3d9bfa0814aaee1ba80b77d56d320
Rich Korneisel <korneisel@cfu.net>
2021-02-18 13:35:01 -0600

    Receipts change

--- src/Receipt.js
+++ src/Receipt.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Receipt.css';
 
 class Receipt extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-    }
-    //render static first to see if shows up on page
-    render(){
+    } 
+    render() {
+        const allToppings = this.props.receipt.order.toppings.map((topping, index) => {
+            return <p key={index}>
+                {topping}
+            </p>
+        })
         console.log(this.props.receipt)
-        return(
+        return (
             <div className="receipt">
                 <h3>{this.props.receipt.person}</h3>
-                <p><span>Main:</span> {this.props.receipt.order.main}</p>
-                <p><span>Protein:</span> {this.props.receipt.order.protein}</p>
-                <p><span>Rice:</span> {this.props.receipt.order.rice}</p>
-                <p><span>Sauce:</span> {this.props.receipt.order.sauce}</p>
-                <p><span>Drink:</span> {this.props.receipt.order.drink}</p>
-                <p><span>Cost:</span> {this.props.receipt.order.cost}</p>
+                <strong><p><span>main</span>: {this.props.receipt.order.main}</p></strong>
+                <strong><p><span>protein</span>: {this.props.receipt.order.protein}</p></strong>
+                <strong><p><span>rice</span>: {this.props.receipt.order.rice}</p></strong>
+                <strong><p><span>sauce</span>: {this.props.receipt.order.sauce}</p></strong>
+                <strong><p><span>drink</span>: {this.props.receipt.order.drink}</p></strong>
+                <strong><p><span>cost</span>: {this.props.receipt.order.cost}</p></strong>                <h4>Toppings</h4>
+                {allToppings}
             </div>
         )
     }
-}
-
-export default Receipt;
+} 
+export default Receipt
